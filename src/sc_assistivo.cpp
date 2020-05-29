2ede6515ef9cb9094329987d1e25a06b61bc92be
AliceM090593 <alice.morosini@mail.polimi.it>
2020-05-29 10:10:42 +0200

    update20200529

--- src/sc_assistivo.cpp
+++ src/sc_assistivo.cpp
@@ -54,50 +54,38 @@ sc_assistivo::~sc_assistivo()
 void sc_assistivo::on_pushButton_salva_clicked()
 {
 
-   // QString   sesso, latodom, lb, la,ROM1_min, ROM1_max, ROM2_min, ROM2_max, ROM3_min, ROM3_max, ROM4_min, ROM4_max, ROM5_min, ROM5_max;
-//    dati::patologia = ui->lineEdit_patologia->text();
-//    sesso = ui->comboBox_sesso->currentText();
-//    latodom = ui->comboBox_latodom->currentText();
-//    lb = ui->lineEdit_lb->text();
-//    la= ui->lineEdit_la->text();
-//    ROM1_min = ui->lineEdit_rom1_min->text();
-//    ROM1_max = ui->lineEdit_rom1_max->text();
-//    ROM2_min = ui->lineEdit_rom2_min->text();
-//    ROM2_max = ui->lineEdit_rom2_max->text();
-//    ROM3_min = ui->lineEdit_rom3_min->text();
-//    ROM3_max = ui->lineEdit_rom3_max->text();
-//    ROM4_min = ui->lineEdit_rom4_min->text();
-//    ROM4_max = ui->lineEdit_rom4_max->text();
-//    ROM5_min = ui->lineEdit_rom5_min->text();
-//    ROM5_max = ui->lineEdit_rom5_max->text();
-//    int band = 1;
-//    if (band ==1) {
+    QString   sesso, latodom, lb, la,ROM1_min, ROM1_max, ROM2_min, ROM2_max, ROM3_min, ROM3_max, ROM4_min, ROM4_max, ROM5_min, ROM5_max;
+    dati::patologia = ui->lineEdit_patologia->text();
+    sesso = ui->comboBox_sesso->currentText();
+    latodom = ui->comboBox_latodom->currentText();
+    lb = ui->lineEdit_lb->text();
+    la= ui->lineEdit_la->text();
+    ROM1_min = ui->lineEdit_rom1_min->text();
+    ROM1_max = ui->lineEdit_rom1_max->text();
+    ROM2_min = ui->lineEdit_rom2_min->text();
+    ROM2_max = ui->lineEdit_rom2_max->text();
+    ROM3_min = ui->lineEdit_rom3_min->text();
+    ROM3_max = ui->lineEdit_rom3_max->text();
+    ROM4_min = ui->lineEdit_rom4_min->text();
+    ROM4_max = ui->lineEdit_rom4_max->text();
+    ROM5_min = ui->lineEdit_rom5_min->text();
+    ROM5_max = ui->lineEdit_rom5_max->text();
 
-//    QSqlQuery demo; ;
-//   prova.prepare("insert into Utenti_ass (Patologia) values ('"+patologia+"') where usernameass = '"+dati::username+"'");
-//   demo.prepare ("update Utenti_ass set Patologia = '"+dati::patologia+"' where usernameass = '"+dati::username+"'");
-//  demo.exec();
-//    if (demo.exec())
-//    {
 
-//        QMessageBox::information(this, tr("done"), tr("done"));
-//    }
 
-//      else {
-//        qDebug()<< demo.lastError().text();
-//        qDebug()<< dati::patologia;
-
-//      }
 
 
 
      QSqlQuery prova;
      prova.prepare("insert into Utenti_ass (Patologia) values ('"+dati::patologia+"')");
      prova.exec();
-
-       QMessageBox::information(this, tr("done"), tr("done"));
-
+     if(prova.exec())
+     {     QMessageBox::information(this, tr("done"), tr("done"));
      }
+     else { qDebug()<<prova.lastError().text();
+
+     }}
+
 
 
 
