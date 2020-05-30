7c7a5ae1d6734f5dc56be373b57f9f89358abd57
AliceM090593 <alice.morosini@mail.polimi.it>
2020-05-30 21:37:07 +0200

    20200530

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -7,40 +7,96 @@
 
 #include <QTableView>
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <string>
+#include <vector>
+using namespace std;
+
 matrix::matrix(QWidget *parent) :
   QDialog(parent),
   ui(new Ui::matrix)
 {
   ui->setupUi(this);
-  // Create a data model for the mapping table from a CSV file
+
+
+
+
+
+//  // Create a data model for the mapping table from a CSV file
   csvModel = new QStandardItemModel(this);
   csvModel->setColumnCount(3);
-  csvModel->setHorizontalHeaderLabels(QStringList() << "x" << "y" << "bool");
+  csvModel->setHorizontalHeaderLabels(QStringList() << "x" << "y"<<"bool");
   ui->tableView_1->setModel(csvModel);
 
-  // load the .csv file
+//  // load the .csv file
+
+
    QFile file("/home/alice/catkin_ws/src/agree_gui/resources/Punti_tappetino.csv");
    if ( !file.open(QFile::ReadOnly | QFile::Text) ) {
        qDebug() << "File not exists";
    }
    else {
-      qDebug() <<  "fatto";
-      QTextStream in(&file);
 
-   while (!in.atEnd())
+      qDebug() <<  "fatto";
+
+
+     QTextStream in(&file);
+
+
+
+
+
+
+   while (!in.atEnd() )
    {
+
        QString line = in.readLine();
-       // Adding to the model in line with the elements
+
+
+
+
+
+
+
+
        QList<QStandardItem *> standardItemsList;
        // consider that the line separated by semicolons into columns
-       for (QString item : line.split(";")) {
-           standardItemsList.append(new QStandardItem(item));
-       }
+
+
+
+      for (QString item : line.split(";")) {
+          standardItemsList.append(new QStandardItem(item));
+
+      }
+
+
         csvModel->insertRow(csvModel->rowCount(), standardItemsList);
+
+
+
    }
+
+
+}
     file.close();
+
+
+
+
+
 }
-}
+
+
+  // altro tentativo
+
+
+
+
+
+
 
 matrix::~matrix()
 {
